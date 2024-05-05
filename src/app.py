ed353b39d6da75d5b38d7ac7a3e1b9ac7db1fd4c
Pruthviraj-2004 <rajsavanur2003@gmail.com>
2024-05-05 15:18:34 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import traceback
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
@@ -8,13 +9,13 @@ from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 import streamlit as st
 
-def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
-  db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-  return SQLDatabase.from_uri(db_uri)
-
-# def init_database() -> SQLDatabase:
-#   db_uri = "mysql+mysqlconnector://root:7516@localhost:3306/technoindustry"
+# def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
+#   db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
 #   return SQLDatabase.from_uri(db_uri)
+
+def init_database() -> SQLDatabase:
+  db_uri = "mysql+mysqlconnector://root:7516@localhost:3306/technoindustry"
+  return SQLDatabase.from_uri(db_uri)
 
 def get_sql_chain(db):
   template = """
@@ -41,6 +42,9 @@ def get_sql_chain(db):
     
   prompt = ChatPromptTemplate.from_template(template)
   
+  # llm = ChatOpenAI(model="gpt-3.5-1106")
+  # gpt-3.5-turbo-0125
+  # llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
   
   def get_schema(_):
     return db.get_table_info()
@@ -52,37 +56,87 @@ def get_sql_chain(db):
     | StrOutputParser()
   )
     
-def get_response(user_query: str, db: SQLDatabase, chat_history: list):
-  sql_chain = get_sql_chain(db)
+# def get_response(user_query: str, db: SQLDatabase, chat_history: list):
+#   sql_chain = get_sql_chain(db)
   
-  template = """
-    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
-    Based on the table schema below, question, sql query, and sql response, write a natural language response.
-    <SCHEMA>{schema}</SCHEMA>
+#   template = """
+#     You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+#     Based on the table schema below, question, sql query, and sql response, write a natural language response.
+#     <SCHEMA>{schema}</SCHEMA>
 
-    Conversation History: {chat_history}
-    SQL Query: <SQL>{query}</SQL>
-    User question: {question}
-    SQL Response: {response}"""
+#     Conversation History: {chat_history}
+#     SQL Query: <SQL>{query}</SQL>
+#     User question: {question}
+#     SQL Response: {response}
+#     Please Note: "if SQL Response is showing an error give a meaningful message on How to write a prompt that the system would understand based on the error.
+#     Please give examples of prompt related to user query.
+#     if there is no error from the sql response then provide the actual result."
+#     """
   
-  prompt = ChatPromptTemplate.from_template(template)
+#   prompt = ChatPromptTemplate.from_template(template)
+#   print(prompt)
   
+#   llm = ChatOpenAI(model="gpt-3.5-turbo-0125", api_key="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")
+#   # llm = ChatOpenAI(model="gpt-3.5-1106")
+#   # llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
   
-  chain = (
-    RunnablePassthrough.assign(query=sql_chain).assign(
-      schema=lambda _: db.get_table_info(),
-      response=lambda vars: db.run(vars["query"]),
-    )
-    | prompt
-    | llm
-    | StrOutputParser()
-  )
+#   chain = (
+#     RunnablePassthrough.assign(query=sql_chain).assign(
+#       schema=lambda _: db.get_table_info(),
+#       response=lambda vars: db.run(vars["query"]),
+#     )
+#     | prompt
+#     | llm
+#     | StrOutputParser()
+#   )
   
-  return chain.invoke({
-    "question": user_query,
-    "chat_history": chat_history,
-  })
+#   return chain.invoke({
+#     "question": user_query,
+#     "chat_history": chat_history,
+#   })
     
+def get_response(user_query: str, db: SQLDatabase, chat_history: list):
+    try:
+        sql_chain = get_sql_chain(db)
+        
+        template = """
+            You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+            Based on the table schema below, question, sql query, and sql response, write a natural language response.
+            <SCHEMA>{schema}</SCHEMA>
+        
+            Conversation History: {chat_history}
+            SQL Query: <SQL>{query}</SQL>
+            User question: {question}
+            SQL Response: {response}
+            Please Note: If the SQL Response is showing an error, provide a meaningful message on how to write a prompt that the system would understand based on the error.
+            """
+        
+        prompt = ChatPromptTemplate.from_template(template)
+        
+        
+        chain = (
+            RunnablePassthrough.assign(query=sql_chain).assign(
+                schema=lambda _: db.get_table_info(),
+                response=lambda vars: db.run(vars["query"]),
+            )
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        
+        return chain.invoke({
+            "question": user_query,
+            "chat_history": chat_history,
+        })
+    
+    except Exception as e:
+        # Provide user-friendly feedback for SQL errors
+        error_message = str(e)
+        if "foreign key constraint" in error_message.lower():
+            return "Sorry, I couldn't delete the vendor because it is associated with other data in the system. Please remove any associated data first, and then try again."
+        else:
+            return "Sorry, something went wrong while processing your request. Please try again or contact support for assistance."
+
   
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
@@ -96,29 +150,29 @@ st.set_page_config(page_title="Chat with MySQL", page_icon=":speech_balloon:")
 st.title("Chat with MySQL")
 
 with st.sidebar:
-    st.subheader("Settings")
-    st.write("This is a simple chat application using MySQL. Connect to the database and start chatting.")
+#     st.subheader("Settings")
+#     st.write("This is a simple chat application using MySQL. Connect to the database and start chatting.")
     
-    st.text_input("Host", value="localhost", key="Host")
-    st.text_input("Port", value="3306", key="Port")
-    st.text_input("User", value="root", key="User")
-    st.text_input("Password", type="password", value="7516", key="Password")
-    st.text_input("Database", value="ipl_prediction", key="Database")
+#     st.text_input("Host", value="localhost", key="Host")
+#     st.text_input("Port", value="3306", key="Port")
+#     st.text_input("User", value="root", key="User")
+#     st.text_input("Password", type="password", value="7516", key="Password")
+#     st.text_input("Database", value="ipl_prediction", key="Database")
     
-    if st.button("Connect"):
-        with st.spinner("Connecting to database..."):
-            db = init_database(
-                st.session_state["User"],
-                st.session_state["Password"],
-                st.session_state["Host"],
-                st.session_state["Port"],
-                st.session_state["Database"]
-            )
-            st.session_state.db = db
-            st.success("Connected to database!")
+#     if st.button("Connect"):
+#         with st.spinner("Connecting to database..."):
+#             db = init_database(
+#                 st.session_state["User"],
+#                 st.session_state["Password"],
+#                 st.session_state["Host"],
+#                 st.session_state["Port"],
+#                 st.session_state["Database"]
+#             )
+#             st.session_state.db = db
+#             st.success("Connected to database!")
 
-      # db = init_database()
-      # st.session_state.db = db
+      db = init_database()
+      st.session_state.db = db
 
 for message in st.session_state.chat_history:
     if isinstance(message, AIMessage):
