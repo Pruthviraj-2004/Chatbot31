a5cf945e875aa39f160afa1c614df26f1b9d2911
Pruthviraj-2004 <103665040+Pruthviraj-2004@users.noreply.github.com>
2024-05-16 00:39:29 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -12,8 +12,8 @@ import os
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
-  # db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-  db_uri = f"mssql+pymssql://{user}:{password}@{host}:{port}/{database}"
+  db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+  # db_uri = f"mssql+pymssql://{user}:{password}@{host}:{port}/{database}"
 
 
   return SQLDatabase.from_uri(db_uri)
